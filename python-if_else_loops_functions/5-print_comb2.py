bcb8122c443d14bb630e09caf5f1a75a00c40736
root <c1zek1011@gmail.com>
2025-11-16 15:12:32 +0000

    full

--- python-if_else_loops_functions/5-print_comb2.py
+++ python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
-for i in range(0, 10):
-    for j in range(0, 10):
-        if i == 9 and j == 9:
-            print("{}{}\n".format(i, j))
-        else:
-            print("{}{}".format(i, j), end=", ")
+for n in range(0, 100):
+    i = n // 10
+    j = n % 10
+
+    if n == 99:
+        print("{}{}\n".format(i, j))
+    else:
+        print("{}{}".format(i, j), end=", ")
