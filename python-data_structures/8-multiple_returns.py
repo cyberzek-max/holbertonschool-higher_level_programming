228f0027ff6450ed39d358f134bb5a058013b849
root <c1zek1011@gmail.com>
2025-11-19 12:32:05 +0000

    data structure

--- python-data_structures/8-multiple_returns.py
+++ python-data_structures/8-multiple_returns.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if len(sentence) == 0:
-        return None
+        return 0, None
     else:
         length = len(sentence)
         first = sentence[0]
-        print("Length: {:d} - First character: {}".format(length, first))
+        return length, first
