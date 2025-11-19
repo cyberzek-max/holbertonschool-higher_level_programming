12d69a1b0875facafe9a23f262ca086cd7adac89
root <c1zek1011@gmail.com>
2025-11-19 09:58:54 +0000

    data structures

--- python-data_structures/2-replace_in_list.py
+++ python-data_structures/2-replace_in_list.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
-    new_list = my_list[:idx]+[element]+my_list[idx+1:]
-    return new_list
+    my_list = my_list[:idx]+[element]+my_list[idx+1:]
+    return my_list
