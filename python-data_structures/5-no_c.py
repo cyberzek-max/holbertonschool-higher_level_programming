d62680f1cc0184937348dd15ec1f37e3a4638f9d
root <c1zek1011@gmail.com>
2025-11-19 10:26:20 +0000

    data structure

--- python-data_structures/5-no_c.py
+++ python-data_structures/5-no_c.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def no_c(my_string):
-    x=""
+    x = ""
     for i in my_string:
-        if i!="c" or i!="C":
-            x+=i
+        if i != "c" or i != "C":
+            x += i
     return x
