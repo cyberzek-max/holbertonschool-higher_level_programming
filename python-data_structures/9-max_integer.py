0cb5c70a14159558850a10cef8dd88d2490fa86b
root <c1zek1011@gmail.com>
2025-11-19 12:35:55 +0000

    data structure

--- python-data_structures/9-max_integer.py
+++ python-data_structures/9-max_integer.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
-def max_integer(my_list=[]):
+def max_integer(my_list = []):
     if len(my_list) == 0:
         return None
     else:
-        m=my_list[0]
+        m = my_list[0]
         for i in my_list:
-            if i>m:
-                m=i
+            if i > m:
+                m = i
         return m
