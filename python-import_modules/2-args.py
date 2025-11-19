05ffe29d79f57a13e12ef6785aada08ffc21cebe
root <c1zek1011@gmail.com>
2025-11-19 08:01:06 +0000

    import modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -1,11 +1,12 @@
 #!/usr/bin/python3
 from sys import argv
-x = argv
-if len(x) == 1:
-    print("0 arguments.")
-elif len(x) == 2:
-    print("1 argument:\n1: ", argv[1])
-else:
-    print("{} arguments:".format(len(x)-1))
-    for i in range(1, len(x)):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    x = argv
+    if len(x) == 1:
+        print("0 arguments.")
+    elif len(x) == 2:
+        print("1 argument:\n1: ", argv[1])
+    else:
+        print("{} arguments:".format(len(x)-1))
+        for i in range(1, len(x)):
+            print("{}: {}".format(i, argv[i]))
