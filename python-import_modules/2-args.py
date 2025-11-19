e135827069e11441f87580f1a695792cb67a7aaf
root <c1zek1011@gmail.com>
2025-11-19 08:04:20 +0000

    import modules

--- python-import_modules/2-args.py
+++ python-import_modules/2-args.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     if len(x) == 1:
         print("0 arguments.")
     elif len(x) == 2:
-        print("1 argument:\n1: ", argv[1])
+        print("1 argument:\n1:", argv[1])
     else:
         print("{} arguments:".format(len(x)-1))
         for i in range(1, len(x)):
