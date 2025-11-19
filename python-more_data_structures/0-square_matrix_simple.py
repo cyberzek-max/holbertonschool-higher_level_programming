6dc22dde55611879ab4b0a9869ab71211776150d
root <c1zek1011@gmail.com>
2025-11-19 13:08:46 +0000

    data structure

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    newmat = matrix[::]
-    for i in range(len(matrix):
-            for x in range(len(matrix[::])):
-                newmat[i][x] = matrix[i][x]**2
+    newmat = list(map(lambda x : x**2, matrix))
     return newmat
