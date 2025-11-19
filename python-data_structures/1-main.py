993a975445b6706f981c827ffd3cddf3617c1559
root <c1zek1011@gmail.com>
2025-11-19 09:35:53 +0000

    data structures

---
+++ python-data_structures/1-main.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def element_at(my_list, idx):
+    if idx<0:
+        print(None)
+    elif idx>=len(my_list):
+        print(None)
+    else:
+        print(my_list[idx])
+    print("Element at index {:d} is {}".format(idx, element_at(my_list, idx)))
