a53385642cb202a23b61af5ef7c6686ad7d986c4
root <c1zek1011@gmail.com>
2025-11-19 10:12:59 +0000

    data structure

--- python-data_structures/4-new_in_list.py
+++ python-data_structures/4-new_in_list.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-def replace_in_list(my_list, idx, element):
+def new_in_list(my_list, idx, element):
     my_list = my_list[:idx]+[element]+my_list[idx+1:]
     new_list = my_list
     return my_list
