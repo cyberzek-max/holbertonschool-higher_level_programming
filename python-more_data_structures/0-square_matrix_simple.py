9882fc4b09033f586dda1e976799aeb6209e89d5
root <c1zek1011@gmail.com>
2025-11-19 13:06:42 +0000

    data structure

---
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+def square_matrix_simple(matrix=[]):
+    newmat = matrix[::]
+    for i in range(len(matrix()):
+            for x in range(len(matrix[::])):
+                newmat[i][x] = matrix[i][x]**2
+    return newmat
