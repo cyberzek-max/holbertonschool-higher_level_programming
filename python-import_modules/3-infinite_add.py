00443bbccbbea4865b4ad11068ab9f2121e7d668
root <c1zek1011@gmail.com>
2025-11-19 08:14:34 +0000

    import modules

---
+++ python-import_modules/3-infinite_add.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+from sys import argv
+if __name__ == "__main__":
+    x = argv
+    c=0
+    if len(x)==1:
+        print(0)
+    else:
+        for i in range(1, len(x)):
+            c+=int(x[i])
+        print(c)
