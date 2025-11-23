39faa00186c1a7ade1e50e37d53ecc51b3c86ded
root <c1zek1011@gmail.com>
2025-11-23 18:52:30 +0000

    error

--- python-exceptions/1-safe_print_integer.py
+++ python-exceptions/1-safe_print_integer.py
@@ -1,11 +1,7 @@
 #!/usr/bin/python3
-def safe_print_integer(my_list=[], x=0):
-    count = 0
-    for i in range(x):
-        try:
-            print("{:d}".format(my_list[i]), end="")
-            count += 1
-        except (ValueError, TypeError):
-            continue
-    print()
-    return count
+def safe_print_integer(value):
+    try:
+        print("{:d}".format(value))
+        return True
+    except Exception:
+        return False
