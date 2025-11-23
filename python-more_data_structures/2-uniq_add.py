c2807714abc4912c947cb881b2f4bf9023425367
root <c1zek1011@gmail.com>
2025-11-23 13:48:09 +0000

    data structure

--- python-more_data_structures/2-uniq_add.py
+++ python-more_data_structures/2-uniq_add.py
@@ -2,7 +2,7 @@
 def uniq_add(my_list=[]):
     list_ = []
     for i in my_list:
-        if not i in list_:
+        if i not in list_:
             list_ += [i]
     summa = 0
     for i in list_:
