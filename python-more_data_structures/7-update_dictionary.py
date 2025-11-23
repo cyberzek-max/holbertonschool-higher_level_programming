a73d256c578c9af8359888c4ef9e78b48d4a54b1
root <c1zek1011@gmail.com>
2025-11-23 14:17:22 +0000

    data structure

---
+++ python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def update_dictionary(a_dictionary, key, value):
+    if key in a_dictionary.keys():
+        for i in a_dictionary.keys():
+            if key == i:
+                a_dictionary[i] == value
+    else:
+        a_dictionary.update(key, value)
+    return a_dictionary
