aa5db46c6fa152fd2b6f1d7e1de338be3f07066c
root <c1zek1011@gmail.com>
2025-11-23 13:25:01 +0000

    data structure

--- python-more_data_structures/0-square_matrix_simple.py
+++ python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-def square_matrix_simple(matrix=[]):
-    newmat = [[ el**2 for el in row ] for row in matrix ]
+def square_matrix_simple( matrix = [] ):
+    newmat = [ [ el**2 for el in row ] for row in matrix ]
     return newmat
