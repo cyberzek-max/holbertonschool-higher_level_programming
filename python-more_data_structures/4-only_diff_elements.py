92e45817f9240b41b280655002c1d6bc49bbabe5
root <c1zek1011@gmail.com>
2025-11-23 13:57:05 +0000

    data structure

--- python-more_data_structures/4-only_diff_elements.py
+++ python-more_data_structures/4-only_diff_elements.py
@@ -6,7 +6,7 @@ def only_diff_elements(set_1, set_2):
             if i == x:
                 list_ += [i]
     list_1 = []
-    for i in [set_1] + [set_2]:
+    for i in [set_1 + set_2]:
         if i not in list_ and i not in list_1:
             list_1 += [i]
     return list_1
