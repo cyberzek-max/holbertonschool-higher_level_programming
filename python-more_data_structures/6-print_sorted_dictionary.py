1a9e29bfae376dbdf161402e80652abee719eb7f
root <c1zek1011@gmail.com>
2025-11-23 14:12:22 +0000

    data structure

--- python-more_data_structures/6-print_sorted_dictionary.py
+++ python-more_data_structures/6-print_sorted_dictionary.py
@@ -2,4 +2,4 @@
 def print_sorted_dictionary(a_dictionary):
     sorted_dict = dict(sorted(a_dictionary.items()))
     for i in sorted_dict.items():
-        print(str(i.key) + ": " + str(i.value()))
+        print(str(i[0]) + ": " + str(i[1]))
