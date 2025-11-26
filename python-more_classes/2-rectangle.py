cdf8223adc4199abcf2e99e679065b159e0ce20e
cyberzek-max <c1zek1011@gmail.com>
2025-11-26 14:34:08 +0400

    classes

--- python-more_classes/2-rectangle.py
+++ python-more_classes/2-rectangle.py
@@ -47,4 +47,6 @@ class Rectangle:
 
     def perimeter(self):
         """Return the perimeter of the Rectangle."""
+        if width == 0 or height == 0:
+            return 0
         return 2 * (self.width + self.height)
