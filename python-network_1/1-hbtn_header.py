b978fc1880274dc6eb5ac754b5205e690e9d9d68
cyberzek-max <c1zek1011@gmail.com>
2025-12-03 11:11:35 +0400

    net1

---
+++ python-network_1/1-hbtn_header.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import urllib.request
+import sys
+"""
+This is the module documentation string.
+It explains what this script does.
+"""
+if __name__ == "__main__":
+    url = sys.argv[1]
+    """
+    This script fetches a URL passed as an argument.
+    """
+    with urllib.request.urlopen(url) as response:
+        headers = response.info()
+        request_id = headers.get('X-Request-Id')
+        print(request_id)
