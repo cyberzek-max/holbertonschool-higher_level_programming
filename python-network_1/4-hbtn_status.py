a2e5ca3fb4e97905981ad69badf71e0db2b4d752
cyberzek-max <c1zek1011@gmail.com>
2025-12-03 14:26:37 +0400

    net1

--- python-network_1/4-hbtn_status.py
+++ python-network_1/4-hbtn_status.py
@@ -10,8 +10,8 @@ if __name__ == "__main__":
     This script fetches a URL passed as an argument.
     """
     if __name__ == "__main__":
-    url = "https://intranet.hbtn.io/status"
-    r = requests.get(url)
-    print("Body response:")
-    print("\t- type: {}".format(type(r.text)))
-    print("\t- content: {}".format(r.text))
+        url = "https://intranet.hbtn.io/status"
+        r = requests.get(url)
+        print("Body response:")
+        print("\t- type: {}".format(type(r.text)))
+        print("\t- content: {}".format(r.text))
