be7f0e762d3536950f51cd2ba691d1bc79571c13
cyberzek-max <c1zek1011@gmail.com>
2025-12-03 11:21:26 +0400

    net1

---
+++ python-network_1/2-post_email.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This is the module documentation string.
+It explains what this script does.
+"""
+import urllib.request
+import sys
+if __name__ == "__main__":
+    """
+    This script fetches a URL passed as an argument.
+    """
+    url = sys.argv[1]
+    email = sys.argv[2]
+    """
+    This script fetches a URL passed as an argument.
+    """
+    data = {"email" : email}
+    encoded_data = urllib.parse.urlencode(data).encode('utf-8')
+    req = urllib.request.Request(url, data=encoded_data, method='POST')
+    with urllib.request.urlopen(req) as response:
+        response_body = response.read()
+        print(response_body)
